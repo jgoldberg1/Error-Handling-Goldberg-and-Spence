3ef0652b64e685ed2769cf616e5098821e56917c
Jenna M Goldberg 9 <jg32137@srcs.loc>
2017-02-15 14:37:47 -0800

    added get and display CO functions. Writing tests for them now

--- functions.js
+++ functions.js
@@ -23,11 +23,79 @@ function between(string, start, end) {
     return string.slice(startAt, endAt);
 }
 
+
+
 /**
- * Returns an area code from a phone number
+ * Returns an area code from a phone number: (###) ###-####
  * @param   {string} phoneNum The phone number
  * @returns {string} The area code
+ * @throws {Error} If the format is incorrect
  */
 function getAreaCode(phoneNum) {
 
+    var areaCode;
+
+    try {
+        areaCode = between(phoneNum, "(", ")");
+        areaCode = areaCode.trim();
+        if (areaCode.length == 3 && Number(areaCode)) {
+            return areaCode;
+        } else {
+            throw new Error("Invalid area code: " + areaCode);
+        }
+    } catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+
+function getCentralCode(phoneNum) {
+     var centralCode;
+
+    try {
+        centralCode = between(phoneNum, "-", "-");
+        centralCode = centralCode.trim();
+        if (centralCode.length == 3 && Number(centralCode))  {
+            return centralCode;
+        } else {
+            throw new Error("Invalid CO code: " + centralCode);
+        }
+    } catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+function displayCentralCode(inputId, outputId) {
+    var outputTextCO = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var centralCode = getCentralCode(phoneNum);
+        outputTextCO = "Your area code is " + centralCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+}
+/**
+ * Displays the area code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayAreaCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var areaCode = getAreaCode(phoneNum);
+        outputText = "Your area code is " + areaCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+    document.getElementById(outputId).innerHTML = outputText;
 }
---
+++ tests.js
@@ -0,0 +1,11 @@
+QUnit.test("Test the getAreaCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getAreaCode(num);
+    assert.deepEqual(result, "415", "Valid area code test passed.");
+});
+
+QUnit.test("Test the getCentralCode function.", function (assert) {
+    var num = "(415)-847-5555";
+    var result = getCentralCode(num);
+    assert.deepEqual(result, "847", "Valid CO code test passed.");
+});
