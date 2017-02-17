f77535903861f4e1abc486d64fadff91ab68c415
Jenna M Goldberg 9 <jg32137@srcs.loc>
2017-02-17 12:57:16 -0800

    trying to add unit tests for display functions

--- tests.js
+++ tests.js
@@ -9,3 +9,11 @@ QUnit.test("Test the getCentralCode function.", function (assert) {
     var result = getCentralCode(num);
     assert.deepEqual(result, "847", "Valid CO code test passed.");
 });
+
+QUnit.test("Test the displayAreaCode function.", function (assert) {
+    var outputText = "",
+    var phoneNum = "(415)-847-5555";
+ document.getElementById('outputId').innerHTML;
+    var result = displayAreaCode(phoneNum, output);
+    assert.deepEqual(result, "415", "display area code test passed.");
+});
